7af9ee976cc6636e4ec3d229fba9202b7bd96ac9
Pat-here <javorev2@gmail.com>
2025-06-25 17:37:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -668,6 +668,10 @@ def init_db():
 
         db.session.commit()
 
+@app.before_first_request
+def initialize_database():
+    init_db()
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
